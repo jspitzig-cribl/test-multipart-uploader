81697d3506b918756213f48225feae042432fdb6
Jeremy Spitzig <jspitzig@cribl.io>
2021-11-10 13:59:48 -0300

    Performance++ - streams

--- index.ts
+++ index.ts
@@ -6,9 +6,10 @@ import { readFile, writeFile, lstat, mkdir } from 'fs/promises'
 import JSZip from 'jszip'
 import { gzip } from 'node-gzip'
 
-import yargs, { command, options } from 'yargs'
+import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import { join } from 'path/posix';
+import { Readable, Stream } from 'stream';
+import { createGzip } from 'zlib';
 
 interface Arguments {
   format: string,
@@ -21,20 +22,6 @@ interface Arguments {
 
 const s3 = new AWS.S3();
 
-async function uploadPart(s3:AWS.S3, partParams:AWS.S3.UploadPartRequest):Promise<UploadPartOutput> {
-  let error;
-  let tryNumber = 0;
-  do {
-    try {
-      error = null;
-      return await s3.uploadPart(partParams).promise()
-    } catch(e) {
-      error = e;
-      tryNumber++;
-      console.error(`Upload part request failed.  Retrying.  Retry #${tryNumber}`)
-    }
-  } while(true);
-}
 function createLine(template:string) : string {
   return template
       .replace(':::replace:::', `:::${uuidv4()}:::${uuidv4()}:::${uuidv4()}:::${uuidv4()}:::`)
@@ -49,7 +36,7 @@ function chunkArray<T>(array:T[], size:number) : T[][] {
   }
   return result;
 }
-async function createLogFile(args:Arguments) : Promise<Buffer> {
+async function createLogFile(args:Arguments) : Promise<Readable> {
   const contents = (await readFile('./source.log')).toString();
   const sourceLines = contents.split('\n');
   const totalSourceLines = sourceLines.length;
@@ -58,74 +45,32 @@ async function createLogFile(args:Arguments) : Promise<Buffer> {
   console.info('Mapping lines');
   const generatedLines = sourceLines.flatMap(l => Array.from({length: repeat}, () => createLine(l)));
   console.info('Building buffer');
-  return chunkArray(generatedLines.slice(0, totalOutputLines), 50000)
-    .reduce((b, ls) => Buffer.concat([b, Buffer.from(`${ls.join('\n')}\n`)]), Buffer.alloc(0));
+  return Readable.from(generatedLines.slice(0, totalOutputLines))
 }
 
-async function createLogZip(args:Arguments) : Promise<Buffer> {
+async function createLogZip(args:Arguments) : Promise<Readable> {
   const zip = new JSZip();
   for(let i = 0; i < args.fileCount; i++) {
     const fileName = `logs/${uuidv4()}.log`;
     const data = await createLogFile(args);
     zip.file(fileName, data);
   }
-  return zip.generateAsync({
-    type: 'nodebuffer'
-  });
+  return new Readable().wrap(zip.generateNodeStream());
 }
 
-async function createLogGZip(args:Arguments) : Promise<Buffer> {
-  const data = await createLogFile(args);
-  return await gzip(data);
+async function createLogGZip(args:Arguments) : Promise<Readable> {
+  return (await createLogFile(args)).pipe(createGzip());
 }
 
-async function getChunk(data:Buffer, chunkSize:number, chunk:number) {
-  const start = chunk * chunkSize;
-  const end = start + chunkSize;
-  if(end >= data.byteLength - 1) {
-    return data.slice(start);
-  } else {
-    return data.slice(start, end)
-  }
-}
-
-async function multipartUploadData(data:Buffer, id:string, ContentType:string) : Promise<void> {
-  const Bucket = 'jspitzig-cribl-test';
-  const Key = `multipart-test/${id}`;
-  const multiPartParams = { Bucket, Key, ContentType };  
-  console.info('Creating multipart upload');
-  const multipart = await s3.createMultipartUpload(multiPartParams).promise();
-  console.info('Created multipart upload');
-  const MultipartUpload : AWS.S3.CompletedMultipartUpload = { Parts: [] };
-  const UploadId = multipart.UploadId!
-  const chunkSize = 5*1024*1024;
-  const chunkCount = Math.ceil(data.byteLength / chunkSize)
-  for(let PartNumber = 1; PartNumber <= chunkCount; PartNumber++) {
-    const Body = await getChunk(data, chunkSize, PartNumber - 1);
-    const partParams :AWS.S3.UploadPartRequest = { Body, Bucket, Key, PartNumber, UploadId, ContentLength:Body.byteLength};
-    console.info(`Uploading part #${PartNumber} of size ${Body.byteLength}`);
-    const uploadResult = await uploadPart(s3, partParams);
-    console.info(`Uploaded part #${PartNumber}`);
-    const ETag = uploadResult.ETag!
-    MultipartUpload.Parts![PartNumber - 1] = { PartNumber, ETag };
-  }
-  const doneParams:AWS.S3.CompleteMultipartUploadRequest = 
-    { Bucket, Key, UploadId, MultipartUpload };
-  console.info(`Completing upload`);
-  await s3.completeMultipartUpload(doneParams).promise();
-  console.info(`Completed upload`);
-}
-
-async function simpleUploadData(data:Buffer, id:string, ContentType:string) : Promise<void> {
+async function uploadData(data:Readable, id:string, ContentType:string, partSize:number = 5*1024*1024) : Promise<void> {
   const Bucket = 'jspitzig-cribl-test';
   const Key = `multipart-test/${id}`;
   await s3.upload({
     Bucket,
     Key,
     ContentType,
-    ContentLength:data.byteLength,
-    Body: data
-  }).promise()
+    Body: data,
+  }, {queueSize: 1, partSize}).promise()
 }
 
 // Multipart
@@ -133,7 +78,7 @@ async function main(args:Arguments) {
   for(let i = 0; (args.repeat < 0) || (i < args.repeat); i++) {
     const uuid = uuidv4();
     console.info('Creating archive');
-    let archive:Buffer;
+    let archive:Readable;
     let contentType:string;
     let fileName:string;
     switch(args.format) {
@@ -169,10 +114,11 @@ async function main(args:Arguments) {
       console.info('Uploading archive');
       switch(args.uploadType) {
         case 'multipart':
-          await multipartUploadData(archive, fileName, contentType);
+          await uploadData(archive, fileName, contentType);
           break;
         case 'simple':
-          await simpleUploadData(archive, fileName, contentType);
+          // If "partSize" is big enough, the upload should be done without multipart
+          await uploadData(archive, fileName, contentType, args.lines * 1000);
           break;
         default: throw new Error('Invalid upload type')
       }
@@ -221,10 +167,10 @@ yargs(hideBin(process.argv))
   })
   .parseAsync()
   .then((yargs) => {
+    console.log('Completed!');
     main(yargs);
   })
-// ().then(function () {
-//   console.log('Completed!');
-// }).catch(function(e) {
-//   console.error('Failed!', e);
-// });
+  .catch(function(e) { 
+    console.error('Failed!', e);
+  });
+
